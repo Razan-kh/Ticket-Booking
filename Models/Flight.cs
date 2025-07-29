09c127535e268e91710602f02b9b40f0b0416672
unknown <you@example.com>
2025-07-30 00:04:20 +0300

    editing booking and flights prop

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -1,10 +1,14 @@
-class Flight {
-    public string Id;
-    public string DepartureCountry;
-    public string DestinationCountry;
-    public DateTime DepartureDate;
-    public string DepartureAirport;
-    public string ArrivalAirport;
-    public Dictionary<FlightClass, int> AvailableSeats;
-    public Dictionary<FlightClass, double> Prices;
-}
+namespace Ticket_Booking.Models;
+
+    public class Flight
+    {
+        public required string Id { get; set; }
+        public required string DepartureCountry { get; set; }
+        public required string DestinationCountry { get; set; }
+        public required DateTime DepartureDate { get; set; }
+        public required string DepartureAirport { get; set; }
+        public required string ArrivalAirport { get; set; }
+        public required Dictionary<FlightClass, int> AvailableSeats { get; set; }
+        public required Dictionary<FlightClass, double> Prices { get; set; }
+    }
+
