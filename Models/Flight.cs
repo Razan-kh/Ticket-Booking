612e6d5971e79ee93a02388fe3deefc3026813f6
unknown <you@example.com>
2025-08-03 17:51:53 +0300

    override toString() method

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -1,14 +1,23 @@
 namespace Ticket_Booking.Models;
 
-    public class Flight
+public class Flight
+{
+    public required string Id { get; set; }
+    public required string DepartureCountry { get; set; }
+    public required string DestinationCountry { get; set; }
+    public required DateTime DepartureDate { get; set; }
+    public required string DepartureAirport { get; set; }
+    public required string ArrivalAirport { get; set; }
+    public required Dictionary<FlightClass, int> AvailableSeats { get; set; }
+    public required Dictionary<FlightClass, double> Prices { get; set; }
+
+    public override string ToString()
     {
-        public required string Id { get; set; }
-        public required string DepartureCountry { get; set; }
-        public required string DestinationCountry { get; set; }
-        public required DateTime DepartureDate { get; set; }
-        public required string DepartureAirport { get; set; }
-        public required string ArrivalAirport { get; set; }
-        public required Dictionary<FlightClass, int> AvailableSeats { get; set; }
-        public required Dictionary<FlightClass, double> Prices { get; set; }
+        
+        return $"ID: {Id} | {DepartureCountry} ➡ {DestinationCountry} on {DepartureDate:yyyy-MM-dd}\n" +
+            $"Airport: {DepartureAirport} ➝ {ArrivalAirport}";
+
+
+    }
     }
 
